db28568b5e236f25a14e55dcfe432c5b5d3a60c9
Oueleed <oueleed@gmail.com>
2019-01-02 06:26:12 +0100

    Setup RecyclerView

--- app/src/main/java/one/jamaa/appjamaa/main/profile/fragment/ProjectsFragment.java
+++ app/src/main/java/one/jamaa/appjamaa/main/profile/fragment/ProjectsFragment.java
@@ -4,18 +4,69 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.DocumentSnapshot;
 
 import one.jamaa.appjamaa.R;
+import one.jamaa.appjamaa.adapter.ProjectsAdapter;
+import one.jamaa.appjamaa.utils.FirestoreHelper;
 
 public class ProjectsFragment extends Fragment {
+
+    private static final String TAG = "ProjectsFragment";
+
+    private ProjectsAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_projects, container, false);
+        FirestoreHelper firestoreHelper = new FirestoreHelper();
+        adapter = new ProjectsAdapter(firestoreHelper.getOptions());
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_projects);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                adapter.deleteItem(viewHolder.getAdapterPosition());
+            }
+        }).attachToRecyclerView(recyclerView);
+        adapter.setOnItemClickListener(new ProjectsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot snapshot, int position) {
+                Log.d(TAG, "onItemClick: Position: " + position);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+        adapter.startListening();
     }
 }
--- app/src/main/java/one/jamaa/appjamaa/utils/FirestoreHelper.java
+++ app/src/main/java/one/jamaa/appjamaa/utils/FirestoreHelper.java
@@ -1,6 +1,84 @@
 package one.jamaa.appjamaa.utils;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+
+import javax.annotation.Nullable;
+
+import one.jamaa.appjamaa.information.Projects;
+
 public class FirestoreHelper {
 
     private static final String TAG = "FirestoreHelper";
+
+    private FirestoreRecyclerOptions<Projects> options;
+    private DocumentReference documentReference;
+    private String userID;
+    private ListenerRegistration listenerRegistration;
+
+    public FirestoreHelper(){
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = firebaseFirestore.collection("Projects");
+        documentReference = collectionReference.document();
+        Query query = collectionReference.orderBy("timestamp", Query.Direction.ASCENDING);
+        options = new FirestoreRecyclerOptions.Builder<Projects>()
+                .setQuery(query, Projects.class)
+                .build();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null){
+            userID = mAuth.getCurrentUser().getUid();
+        }
+    }
+
+    public void addProject(Projects project){
+        Log.d(TAG, "addProject: ");
+        documentReference.set(project).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d(TAG, "onSuccess: ");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: " + e.getMessage());
+            }
+        });
+    }
+
+    public void onStart(){
+        Log.d(TAG, "onStart: ");
+        listenerRegistration = documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null){
+                    Log.d(TAG, "onEvent: " + e.getMessage());
+                    return;
+                }
+                if (documentSnapshot.exists()){
+                    Log.d(TAG, "onEvent: ");
+                }
+            }
+        });
+    }
+
+    public void onStop(){
+        listenerRegistration.remove();
+    }
+
+    public FirestoreRecyclerOptions<Projects> getOptions() {
+        return options;
+    }
 }
