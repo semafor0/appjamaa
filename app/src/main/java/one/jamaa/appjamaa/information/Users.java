d11a082326ee103648a8f9c0cfea898dc4bd5ba3
semafor0 <ibrahimludwick@gmail.com>
2019-01-09 18:03:25 +0700

    toolbar moved to new contract activity

--- app/src/main/java/one/jamaa/appjamaa/information/Users.java
+++ app/src/main/java/one/jamaa/appjamaa/information/Users.java
@@ -1,5 +1,59 @@
 package one.jamaa.appjamaa.information;
 
-public class Users {
+public class Users
+
+
+{
+
+/* For the card
+
+
+    string username;
+
+    date joindate;
+
+    int participated;  (participated = managed + supported)
+
+    int managed;
+
+    double profit;
+
+    int invested;
+
+    int trust;
+
+*/
+
+
+/* For the profile only
+
+
+
+   int managed;
+
+   int supported;
+
+   double success;
+
+   int following;
+
+   int followers;
+
+   string location;
+
+   string profession;
+
+   string education;
+
+
+
+
+ */
+
+
+
+
+
+
 
 }
